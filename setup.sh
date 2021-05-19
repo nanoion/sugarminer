5b5dbca0c6a6ddaa360e4ba3007f154df9649c26
Poi <nanoion@gmail.com>
2021-05-19 13:51:59 +0700

    init

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pkg install automake clang git vim
+git clone https://github.com/decryp2kanon/sugarmaker.git ../
+cd ../sugarmaker
+./autogen.sh
+./configure CFLAGS="-Wall -O2 -fomit-frame-pointer" CXXFLAGS="$CFLAGS -std=gnu++11"
+make
+HOSTNAME=$(getprop net.hostname)
+echo "./sugarmaker -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qvartnefrf4nwnve0fh23harxtnl34lpcv06gnw.${HOSTNAME} -t 2" > autominer.sh
+chmod a+x autominer.sh
